38b3bca282e8290343acfaa4d410a88136e58fe8
Nikita Sviridov <nikswirlo@gmail.com>
2026-09-06 23:33:35 +0300

    Add quiz topics 04 through 06

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "data-science-quiz-v1";
+const CACHE = "data-science-quiz-v2";
 const LOCAL = ["./", "./index.html", "./quiz.html", "./styles.css", "./quiz.js", "./cards.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(LOCAL))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
