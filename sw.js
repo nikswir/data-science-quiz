79eb5b87520637b0cb7ed1d9cf1edcc2201ba0fc
Nikita Sviridov <nikswirlo@gmail.com>
2026-09-07 14:03:06 +0300

    Add manual Russian card translations

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "data-science-quiz-v7";
-const LOCAL = ["./", "./index.html", "./quiz.html", "./manage.html", "./edit.html", "./styles.css", "./quiz.js", "./cards.js", "./card-store.js", "./cloud-sync.js", "./supabase-config.js", "./home.js", "./manage.js", "./edit.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "data-science-quiz-v8";
+const LOCAL = ["./", "./index.html", "./quiz.html", "./manage.html", "./edit.html", "./styles.css", "./quiz.js", "./cards.js", "./cards-ru.js", "./card-store.js", "./cloud-sync.js", "./supabase-config.js", "./home.js", "./manage.js", "./edit.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(LOCAL))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener("fetch", event => {
